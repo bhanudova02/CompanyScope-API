98ce51f0e7bd90252045d014e65cb77324a23606
bhanudova02 <bhanudova02@gmail.com>
2025-10-19 11:48:26 +0530

    env changes added

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://bhanudova02_db_user:0YVqvExt6sdbd8oI@cluster0.9pncqpg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/companies-directory', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
